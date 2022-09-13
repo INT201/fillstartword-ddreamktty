e690230b76c17a7815bf873fa776de4d313e392e
ddreamktty <kittiya.dreamktty@mail.kmutt.ac.th>
2022-09-14 03:49:45 +0700

    commit1

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,16 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
+  let x = startWord
+  let y = word
+  if(y === null || y === undefined) {
+    return undefined
+  }
+  else if(y.startsWith(x)) {
+    return y
+  }else{
+    return x+y
+  }
   //code here
 }
 module.exports = fillStartWord
